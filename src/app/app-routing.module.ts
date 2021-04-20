55cd9d94465c031fd5c04313989493643f50544f
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-04-20 12:04:41 -0300

    Coloquei a pagina de Login como a root page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'tela-login',
     pathMatch: 'full'
   },
   {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TelaLoginPage } from './tela-login/tela-login.page';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  rootPage:any = "tela-login";
+  rootPage:any = "TelaLoginPage";
 
   constructor() {}
 }
