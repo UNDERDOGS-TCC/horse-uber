130b7ca48ec5188f7c35ba3e10ab4493318c8951
ouwargui <guikemon2@gmail.com>
2021-04-25 16:40:41 -0300

    arrumamos a tela preta e o botão de criar conta

--- src/app/make-a-ride/make-a-ride.page.ts
+++ src/app/make-a-ride/make-a-ride.page.ts
@@ -1,7 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { ConnectivityService } from 'src/app/connectivity-service.service';
-import { Geolocation } from '@ionic-native/geolocation';
 
 
 
--- src/app/your-trips/your-trips.page.ts
+++ src/app/your-trips/your-trips.page.ts
@@ -1,43 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { model, Schema, Model, Document } from 'mongoose';
 
 @Component({
   selector: 'app-your-trips',
   templateUrl: './your-trips.page.html',
   styleUrls: ['./your-trips.page.scss'],
 })
+export class YourTripsPage implements OnInit {
+  rides: any;
 
-
- export class YourTripsPage implements OnInit {
- rides: any;
- constructor() {
-
- }
-  getData(){
-
+  constructor(public http: HttpClient) {
+    this.getData();
   }
- ngOnInit() {
- }
- }
 
-// export class YourTripsPage implements OnInit {
+  getData() {
+    this.http
+      .get(
+        'https://my-json-server.typicode.com/CaiqueSobral/horse_uberjsontestes/rides'
+      )
+      .subscribe(
+        (data) => {
+          this.rides = data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 
-// rides: any;
-
-// constructor(public http: HttpClient) {
-//   this.getData();
-// }
-
-//  getData(){
-//    this.http.get("https://my-json-server.typicode.com/CaiqueSobral/horse_uberjsontestes/rides").subscribe((data) => {
-//      this.rides = data
-//    }, (error) => {
-//      console.log(error)
-//    })
-//  }
-
-// ngOnInit() {
-// }
-
-// }
+  ngOnInit() {}
+}
