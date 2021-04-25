db9092c78f7c4d1122d61eb2cee5369a1cdf3597
ouwargui <guikemon2@gmail.com>
2021-04-25 18:55:36 -0300

    terminei a pagina de make-a-ride e estruturei o mapa do google maps

--- src/app/make-a-ride/make-a-ride.page.ts
+++ src/app/make-a-ride/make-a-ride.page.ts
@@ -1,14 +1,55 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { NavController } from '@ionic/angular';
-
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-make-a-ride',
   templateUrl: './make-a-ride.page.html',
   styleUrls: ['./make-a-ride.page.scss'],
 })
-export class MakeARidePage {
-  constructor() {}
+export class MakeARidePage implements OnInit {
+  map: GoogleMap;
+  constructor(private platform: Platform) {}
 
+  async ngOnInit() {
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+  loadMap() {
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
 }
