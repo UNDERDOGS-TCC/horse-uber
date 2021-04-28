162a4639df87fca5ff2052cb0706127e828719c1
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-04-28 12:55:32 -0300

    adicionando o serviço de conexão do cordova

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { GoogleMaps } from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap } from '@ionic-native/google-maps';
+import { ConnectivityserviceService } from './connectivityservice.service';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-  GoogleMaps],
+  GoogleMaps, GoogleMap, ConnectivityserviceService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
