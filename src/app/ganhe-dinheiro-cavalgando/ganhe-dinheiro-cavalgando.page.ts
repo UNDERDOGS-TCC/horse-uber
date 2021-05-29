bb6881e89f87aedd8c4db009d42797353ceb6cb1
William Vitor <theluckc@outlook.com>
2021-05-29 17:54:37 -0300

    Tela de cavaleiro implementada

--- src/app/ganhe-dinheiro-cavalgando/ganhe-dinheiro-cavalgando.page.ts
+++ src/app/ganhe-dinheiro-cavalgando/ganhe-dinheiro-cavalgando.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ganhe-dinheiro-cavalgando',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GanheDinheiroCavalgandoPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirmação',
+      subHeader: 'Obrigado por usar Horse Uber',
+      message: 'Seus dados foram recebidos! Dentro de 15 dias você receberá uma confirmação no email fornecido.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
 }
